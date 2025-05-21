4bdd79853683c213da44be8d3c1fee0d0427ca14
Marvin046 <info@staycoolairco.nl>
2025-05-21 16:09:36 +0200

    Fix import issue in applications page using dynamic import

--- src/app/[locale]/applications/page.tsx
+++ src/app/[locale]/applications/page.tsx
@@ -1,6 +1,9 @@
 import { unstable_setRequestLocale } from 'next-intl/server'
 import type { Metadata } from 'next'
-import ApplicationsClient from './ApplicationsClient'
+import dynamic from 'next/dynamic'
+
+// Use dynamic import with no SSR to avoid hydration issues with client components
+const ApplicationsClient = dynamic(() => import('./ApplicationsClient'), { ssr: false })
 
 // Generate static params for locales
 export function generateStaticParams() {
